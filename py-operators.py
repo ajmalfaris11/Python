4a0aebe5570857fb9bf2748d1af82b85eec63bd6
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-06 12:02:30 +0530

    Examples for python arithemetic operators

--- py-operators.py
+++ py-operators.py
@@ -9,3 +9,55 @@
 #  '**'   Exponentiation
 #  '//'   Floor Devider
 
+
+
+# ADDITION EXAMPLE
+# ----------------
+a = 10
+b = 5
+c = a + b
+print("Addition: 10 + 5 = ", c)
+
+# SUBTRACTION EXAMPLE
+# -------------------
+a = 10
+b = 5
+c = a - b
+print("Subtraction: 10 - 5 = ", c)
+
+# MULTIPLICATION EXAMPLE
+# ----------------------
+a = 10
+b = 5
+c = a - b
+print("Multiplication: 10 X 5 = ", c)
+
+# DIVITION EXAMPLE
+# -------------------
+a = 10
+b = 5
+c = a / b
+print("Multiplication: 10 / 5 = ", c)
+
+# MODULUS EXAMPLE
+# ---------------
+a = 10
+b = 5
+c = a % b
+print("Modulus: 10 % 5 = ", c)
+
+# EXPONENTIATION EXAMPLE
+# ----------------------
+a = 10
+b = 5
+c = a ** b
+print("Multiplication: 10 ** 5 = ", c)
+
+# FLOOR DIVIDER EXAMPLE
+# ---------------------
+a = 10
+b = 5
+c = a // b
+print("Multiplication: 10 // 5 = ", c)
+
+# i) the Floor Divider operation using for avoid decimels
