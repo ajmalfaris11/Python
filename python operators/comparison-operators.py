06dbf8e242867ac19a44da81bc8e93742aeb177e
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-07 12:12:32 +0530

    PYTHON COMPARISON OPERATORS EXAMPLES

--- python operators/comparison-operators.py
+++ python operators/comparison-operators.py
@@ -7,3 +7,45 @@
 #'  <      ->     Less than         ->   Returns True if the left side is less
 #'  >=     ->     Greater than or equal to -> Returns True if the left side is greater or equal
 #'  <=     ->     Less than or equal to    -> Returns True if the left side is less or equal
+
+
+
+
+# PYTHON COMPARISON OPERATORS EXAMPLES
+# *************************************
+
+# EQUAL TO EXAMPLE
+# -----------------
+a = 10
+b = 5
+print("Equal To: a == b ->", a == b)  # False
+
+# NOT EQUAL TO EXAMPLE
+# ---------------------
+a = 10
+b = 5
+print("Not Equal To: a != b ->", a != b)  # True
+
+# GREATER THAN EXAMPLE
+# ---------------------
+a = 10
+b = 5
+print("Greater Than: a > b ->", a > b)  # True
+
+# LESS THAN EXAMPLE
+# ------------------
+a = 10
+b = 5
+print("Less Than: a < b ->", a < b)  # False
+
+# GREATER THAN OR EQUAL TO EXAMPLE
+# ---------------------------------
+a = 10
+b = 5
+print("Greater Than or Equal To: a >= b ->", a >= b)  # True
+
+# LESS THAN OR EQUAL TO EXAMPLE
+# ------------------------------
+a = 10
+b = 10
+print("Less Than or Equal To: a <= b ->", a <= b)  # True
