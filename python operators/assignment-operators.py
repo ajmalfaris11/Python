76517dde2e1f971b59ddbf997016a2db3f8dc55c
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-07 12:11:22 +0530

    PYTHON ASSIGNMENT OPERATORS EXAMPLES

--- python operators/assignment-operators.py
+++ python operators/assignment-operators.py
@@ -9,3 +9,55 @@
 #'  %=     ->     X %= 10     ->      X = X%10
 #'  //=    ->     X //= 10    ->      X = X//10
 #'  **=    ->     X **= 10    ->      X = X**10
+
+
+
+# PYTHON ASSIGNMENT OPERATORS EXAMPLES
+# *************************************
+
+# ASSIGNMENT EXAMPLE
+# -------------------
+x = 10
+print("Assignment: x = 10 -> x =", x)
+
+# ADDITION ASSIGNMENT EXAMPLE
+# ----------------------------
+x = 10
+x += 5  # x = x + 5
+print("Addition Assignment: x += 5 -> x =", x)
+
+# SUBTRACTION ASSIGNMENT EXAMPLE
+# -------------------------------
+x = 10
+x -= 5  # x = x - 5
+print("Subtraction Assignment: x -= 5 -> x =", x)
+
+# MULTIPLICATION ASSIGNMENT EXAMPLE
+# ----------------------------------
+x = 10
+x *= 5  # x = x * 5
+print("Multiplication Assignment: x *= 5 -> x =", x)
+
+# DIVISION ASSIGNMENT EXAMPLE
+# ----------------------------
+x = 10
+x /= 5  # x = x / 5
+print("Division Assignment: x /= 5 -> x =", x)
+
+# MODULUS ASSIGNMENT EXAMPLE
+# ---------------------------
+x = 10
+x %= 6  # x = x % 6
+print("Modulus Assignment: x %= 6 -> x =", x)
+
+# FLOOR DIVISION ASSIGNMENT EXAMPLE
+# ----------------------------------
+x = 10
+x //= 3  # x = x // 3
+print("Floor Division Assignment: x //= 3 -> x =", x)
+
+# EXPONENTIATION ASSIGNMENT EXAMPLE
+# ----------------------------------
+x = 10
+x **= 2  # x = x ** 2
+print("Exponentiation Assignment: x **= 2 -> x =", x)
