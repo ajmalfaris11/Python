f03d2657935cecdb1c5c377626683e859fafa506
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-08 12:10:12 +0530

    PYTHON COMPARISON OPERATORS

---
+++ python operators/comparison-operators.py
@@ -0,0 +1,9 @@
+# PYTHON COMPARISON OPERATORS
+# ****************************
+
+#'  ==     ->     Equal to          ->   Returns True if both sides are equal
+#'  !=     ->     Not equal to      ->   Returns True if both sides are not equal
+#'  >      ->     Greater than      ->   Returns True if the left side is greater
+#'  <      ->     Less than         ->   Returns True if the left side is less
+#'  >=     ->     Greater than or equal to -> Returns True if the left side is greater or equal
+#'  <=     ->     Less than or equal to    -> Returns True if the left side is less or equal
