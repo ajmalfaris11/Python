1fd67ada080953821d9eff3bccb3439175ed779d
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-09 12:11:22 +0530

    PYTHON LOGICAL OPERATORS

---
+++ python operators/logical-operators.py
@@ -0,0 +1,6 @@
+# PYTHON LOGICAL OPERATORS
+# *************************
+
+#'  and   -> Logical AND      -> Returns True if both conditions are True
+#'  or    -> Logical OR       -> Returns True if at least one condition is True
+#'  not   -> Logical NOT      -> Reverses the result of a condition (True becomes False, and vice versa)
