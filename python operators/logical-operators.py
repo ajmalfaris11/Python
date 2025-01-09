08bdd0378f83485b0657b2bb279b4a599381abf2
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-09 12:12:42 +0530

    PYTHON LOGICAL OPERATORS EXAMPLES

--- python operators/logical-operators.py
+++ python operators/logical-operators.py
@@ -4,3 +4,33 @@
 #'  and   -> Logical AND      -> Returns True if both conditions are True
 #'  or    -> Logical OR       -> Returns True if at least one condition is True
 #'  not   -> Logical NOT      -> Reverses the result of a condition (True becomes False, and vice versa)
+
+
+
+
+
+
+
+
+# PYTHON LOGICAL OPERATORS EXAMPLES
+# **********************************
+
+# LOGICAL AND EXAMPLE
+# --------------------
+a = 10
+b = 20
+c = 30
+print("Logical AND: (a < b) and (b < c) ->", (a < b) and (b < c))  # True
+
+# LOGICAL OR EXAMPLE
+# -------------------
+a = 10
+b = 20
+c = 5
+print("Logical OR: (a < b) or (b < c) ->", (a < b) or (b < c))  # True
+
+# LOGICAL NOT EXAMPLE
+# --------------------
+a = 10
+b = 20
+print("Logical NOT: not(a > b) ->", not(a > b))  # True
