23a9c3e53e13fe1e0d38c985dac96d76f1ffcf93
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-14 18:43:33 +0530

    Python If statement

---
+++ python if-else/statement.py
@@ -0,0 +1,29 @@
+a = 5
+if a > 0 :
+     print("It's a Positive Number")
+elif a < 0 :
+     print("It's Negative Number")
+else:
+     print("It's a ZERO")
+
+
+        
+b = 8
+# NESTED IF ELSE
+if b >= 0 :
+     if b > 0 :
+          print("It's a Positive Number")
+     else:
+          print("It's ZERO")
+else:
+     print("It's a Negative Number")
+
+
+
+
+#  If state ment with logical operators
+c = 15
+if c > 1 and a < 18:
+     print("He is Young")
+
+
