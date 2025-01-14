4529378aad5dc161c53053ad0c6c6d21c90e44c4
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-14 20:38:43 +0530

    python function

---
+++ python function/function.py
@@ -0,0 +1,25 @@
+def message(): # define the function
+    print("Hello World")
+
+message() # call the function 
+
+
+
+# function with parameters
+def replay(name):
+    print(name, "Replied")
+
+replay("ajmal")
+
+
+# find sum functionality
+def find_sum(num1, num2):
+    return(num1 + num2)
+
+print(find_sum (10, 20))
+
+# Find Squre Function
+def find_square(num):
+    return num*num
+
+print(find_square(2))
