a7013a431666fb13ef90251c48d544c600dc4f49
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-22 20:51:12 +0530

    Python Mudule Examples, we can create unlimited functions inside a module

---
+++ python modules/main.py
@@ -0,0 +1,5 @@
+import myModule # imported our module
+
+myModule.name("Ajmal")
+
+myModule.age(22)
---
+++ python modules/myModule.py
@@ -0,0 +1,5 @@
+def name(name):
+    print("Hello My name is", name)
+
+def age(age):
+    print("I am ", age, " years old")
