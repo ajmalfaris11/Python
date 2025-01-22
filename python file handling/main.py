d7266ef246a7fd136e5b274451a2330abcf6d19b
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-22 21:27:18 +0530

    python file handling | read the file

--- python file handling/main.py
+++ python file handling/main.py
@@ -1,3 +1,8 @@
 f = open("C:/python/py-language/python file handling/sample.txt", "w")
 f.write("Learn Python Programming")
 f.close()
+
+f = open("C:/python/py-language/python file handling/sample.txt", "r")
+read = f.read()
+print(read)
+f.close()
