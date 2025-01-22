f960bae48911a92598398dbc5746e1b5abbe27d3
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-01-22 21:06:21 +0530

    Example of take intiger input and find the sum

---
+++ python input/calc.py
@@ -0,0 +1,11 @@
+print("""
+      Let's find the sum of 2 numbers
+      -------------------------------
+      """)
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+sum = num1+num2
+
+print(num1, " + ", num2, " = ", sum)
